4b09184d2cf5ac297af4ad147dbfa3bb2f0adf97
ggoldsht@stevens.edu <ggoldsht@stevens.edu>
2018-02-26 13:29:40 -0500

    Fixing select assignments

--- Web and Rest/web/app/routes/home/assign.js
+++ Web and Rest/web/app/routes/home/assign.js
@@ -350,7 +350,7 @@ export default Ember.Route.extend({
     },
     selectAssignment: function(index) {
 		console.log(index);
-		console.log(assignments[index]);
+		console.log(this.assignments[index]);
       var employees = this.get('employees');
       var assignment = this.get('assignments')[index];
       this.set('selected', index);
