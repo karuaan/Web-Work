3f00ac5d19baf6626645bfa08c3172453a4aa54b
ggoldsht@stevens.edu <ggoldsht@stevens.edu>
2018-02-26 21:19:25 -0500

    Fixing parse.js

--- Web and Rest/restServer/index.js
+++ Web and Rest/restServer/index.js
@@ -113,7 +113,8 @@ app.get('/test/getallusers', function(req, res){
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200
+	res.headers.delete('Content-Type');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
