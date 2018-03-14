4cc5390c05fe29124bcc8a6697df75198ed294b8
Gregory Goldshteyn <ggoldsht@stevens.edu>
2018-03-13 21:55:33 -0400

    Master table post operation

---
+++ Web and Rest/angular/src/app/master-entry.ts
@@ -0,0 +1,11 @@
+export class MasterEntry {
+	USER_ID: Number;
+	GROUP_ID: Number;
+	USER_FIRST_NAME: string;
+	USER_LAST_NAME: string;
+	USER_EMAIL: string;
+	GROUP_NAME: string;
+	ASSIGNMENT_ID: Number;
+	IS_COMPLETE: Object;
+	ASSIGNMENT_NAME: string;
+}
