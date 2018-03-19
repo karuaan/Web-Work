0020c5d845114df26d059da79d471531fad92b84
Goldshteyn Gregory <q446618@americas.bmw.corp>
2018-03-19 10:06:18 -0400

    adding modals

---
+++ Web and Rest/angular/src/app/book.ts
@@ -0,0 +1,5 @@
+export class Book {
+	NAME: string;
+	URL: string;
+	ID: Number;
+}
---
+++ Web and Rest/angular/src/app/lesson.ts
@@ -0,0 +1,7 @@
+export class Lesson {
+	ID: Number;
+	NAME: string;
+	BOOK_ID: Number;
+	PAGE_START: Number;
+	PAGE_END: Number;
+}
