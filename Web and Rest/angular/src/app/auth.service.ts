b6dbdf7f5c5030d03a12610fdf7af76b60168780
ggoldsht@stevens.edu <ggoldsht@stevens.edu>
2018-03-28 04:25:35 -0400

    adding firebase login

--- Web and Rest/angular/src/app/auth.service.ts
+++ Web and Rest/angular/src/app/auth.service.ts
@@ -1,6 +1,5 @@
 // src/app/auth.service.ts
 import { Injectable } from '@angular/core';
-import { Router } from "@angular/router";
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/Observable';
