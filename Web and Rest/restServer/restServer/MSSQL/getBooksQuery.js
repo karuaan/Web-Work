f76253587abce0cf7ce75567361d199d7dd87f49
karan <karuaan@gmail.com>
2018-05-07 02:50:10 -0400

    Added bookservices

---
+++ Web and Rest/restServer/restServer/MSSQL/getBooksQuery.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+
+var getBooksQuery = function(callback) {
+
+    var config = {
+        "server": "microsoft-sql-database.cgkepgzez06k.us-east-2.rds.amazonaws.com",
+        "user": "admin",
+        "password": "password",
+        "database": "READER"
+    };
+
+    sql.connect(config, err => {
+        new sql.Request().query('SELECT * FROM BOOKS', (err, results) => {
+            if(err) {
+                callback(err, null);
+            }
+            else {
+                callback(null, results['recordset']);
+            }
+        })
+    });
+};
+
+
+exports.query = getBooksQuery;
