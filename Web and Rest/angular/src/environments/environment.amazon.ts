5df82a498d2cae9369402919058d01cdb8a24f17
Gregory Goldshteyn <ggoldsht@stevens.edu>
2018-05-14 22:02:03 -0400

    changed amazon environment url

--- Web and Rest/angular/src/environments/environment.amazon.ts
+++ Web and Rest/angular/src/environments/environment.amazon.ts
@@ -4,7 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-   production: true,
    firebase: {
     apiKey: "AIzaSyDj3uGXUayslSgPJnwmpqHjwQ_c0ZCqBv4",
     authDomain: "safety-book-reader.firebaseapp.com",
