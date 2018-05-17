3287bd1f25f443a4ac61e5eb98b5dfc9a42de20d
Gregory Goldshteyn <ggoldsht@stevens.edu>
2018-05-17 04:11:43 -0400

    added err to getgroups route

--- Web and Rest/restServer/index.js
+++ Web and Rest/restServer/index.js
@@ -2040,7 +2040,7 @@ app.post('/getAssignmentsUser', function(req, res){
 app.post('/getgroups',function(req,res){
 
 	console.log(req.body.admin_id)
-	con.query("SELECT DISTINCT ID, NAME FROM GROUPS WHERE ADMIN_ID = "+mysql.escape(req.body.admin_id),function(err,data,fields){
+	con.query("SELECT DISTINCT ID, NAME FROM GROUPS WHERE ADMIN_ID=?",[req.body.admin_id],function(err,data,fields){
 		if(!err){
 			res.json(data);
 		}
